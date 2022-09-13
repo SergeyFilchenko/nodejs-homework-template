52f5b36e96589ecb46584e0912980c5338ef276b
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-09-13 22:05:45 +0300

    auth

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api/v1/auth", authRouter);
+app.use("/api/auth", authRouter);
 
 app.use("/api/contacts", contactsRouter);
 
