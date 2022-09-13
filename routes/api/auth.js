af4630826c32e61a4e1257015cbf85faccb462eb
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-09-13 20:57:45 +0300

    add routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,26 +1,54 @@
-const express = require('express');
+const express = require("express");
 
 const { joiSchema, joiSubscrSchema } = require("../../models/user");
-const { controllerWrapper, validation, authenticate } = require('../../middlewares');
-const upload = require('../../middlewares/upload');
+const {
+  controllerWrapper,
+  validation,
+  authenticate,
+} = require("../../middlewares");
+const upload = require("../../middlewares/upload");
 const { auth: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
 module.exports = router;
 
-router.post("/users/signup", validation(joiSchema), controllerWrapper(ctrl.signup));
+router.post(
+  "/users/signup",
+  validation(joiSchema),
+  controllerWrapper(ctrl.signup)
+);
 
 router.get("/users/verify/:verifyToken", controllerWrapper(ctrl.verify));
 
-router.post("/users/login", validation(joiSchema), controllerWrapper(ctrl.login));
+router.post(
+  "/users/login",
+  validation(joiSchema),
+  controllerWrapper(ctrl.login)
+);
 
-router.patch('/users', authenticate, validation(joiSubscrSchema), controllerWrapper(ctrl.updateSubscr));
+router.post(
+  "/verify",
+  validationBody(schemas.verifyEmailSchema),
+  controllerWrapper(ctrl.resendVerifyEmail)
+);
+
+router.patch(
+  "/users",
+  authenticate,
+  validation(joiSubscrSchema),
+  controllerWrapper(ctrl.updateSubscr)
+);
 
 router.get("/users/logout", authenticate, controllerWrapper(ctrl.logout));
 
 router.get("/users/current", authenticate, controllerWrapper(ctrl.current));
 
-router.patch('/users/avatars', authenticate, upload.single('avatar'), ctrl.avatars);
+router.patch(
+  "/users/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.avatars
+);
 
 module.exports = router;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { controllerWrapper, validation, authenticate } = require('../../middlewares');
-const {contactsController: ctrl} = require('../../controllers');
-const { joiSchema, updateFavoriteJoiSchema } = require('../../models/contact');
+const {
+  controllerWrapper,
+  validation,
+  authenticate,
+} = require("../../middlewares");
+const { contactsController: ctrl } = require("../../controllers");
+const { joiSchema, updateFavoriteJoiSchema } = require("../../models/contact");
 
-router.get('/', authenticate, controllerWrapper(ctrl.listContacts));
+router.get("/", authenticate, controllerWrapper(ctrl.listContacts));
 
-router.get('/:contactId', authenticate, controllerWrapper(ctrl.getContactById));
+router.get("/:contactId", authenticate, controllerWrapper(ctrl.getContactById));
 
-router.post('/', authenticate, validation(joiSchema), controllerWrapper(ctrl.addContact));
+router.post(
+  "/",
+  authenticate,
+  validation(joiSchema),
+  controllerWrapper(ctrl.addContact)
+);
 
-router.delete('/:contactId', authenticate, controllerWrapper(ctrl.removeContact));
+router.post(
+  "/verify",
+  validationBody(schemas.verifyEmailSchema),
+  controllerWrapper(ctrl.resendVerifyEmail)
+);
 
-router.put('/:contactId', authenticate, validation(joiSchema), controllerWrapper(ctrl.updateContact));
+router.delete(
+  "/:contactId",
+  authenticate,
+  controllerWrapper(ctrl.removeContact)
+);
 
-router.patch("/:contactId", authenticate, validation(updateFavoriteJoiSchema), controllerWrapper(ctrl.updateFavoriteContact));
+router.put(
+  "/:contactId",
+  authenticate,
+  validation(joiSchema),
+  controllerWrapper(ctrl.updateContact)
+);
+
+router.patch(
+  "/:contactId",
+  authenticate,
+  validation(updateFavoriteJoiSchema),
+  controllerWrapper(ctrl.updateFavoriteContact)
+);
 
 module.exports = router;
-
